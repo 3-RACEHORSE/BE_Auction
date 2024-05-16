bec79df808ce41d51312e20c23a87e732b5f5495
ddolboghi <rhdfyd128@gmail.com>
2024-05-16 14:28:54 +0900

    feat: eureka-client annotation 적용

--- src/main/java/com/skyhorsemanpower/auction/AuctionApplication.java
+++ src/main/java/com/skyhorsemanpower/auction/AuctionApplication.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableMongoRepositories
-//@RefreshScope
-//@EnableDiscoveryClient
+@RefreshScope
+@EnableDiscoveryClient
 public class AuctionApplication {
 
 	public static void main(String[] args) {
