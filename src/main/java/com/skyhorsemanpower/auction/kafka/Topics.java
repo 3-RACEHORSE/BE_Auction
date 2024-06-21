ad7b5d017350121252f8d43f8d5d7797dc56bccb
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-21 15:59:43 +0900

    refactor: topic 이름을 auction-close-topic 로 수정(#192)

--- src/main/java/com/skyhorsemanpower/auction/kafka/Topics.java
+++ src/main/java/com/skyhorsemanpower/auction/kafka/Topics.java
@@ -22,7 +22,7 @@ public enum Topics {
         public static final String PAYMENT_SERVICE = "event-preview-topic";
         public static final String SUCCESSFUL_BID_ALARM = "successful-bid-alarm-topic";
         public static final String INITIAL_AUCTION = "initial-auction-topic";
-        public static final String AUCTION_CLOSE_STATE = "auction-close-state-topic";
+        public static final String AUCTION_CLOSE_STATE = "auction-close-topic";
 
     }
 
