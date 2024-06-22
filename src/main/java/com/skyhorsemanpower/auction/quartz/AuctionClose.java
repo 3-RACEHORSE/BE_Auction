d75b5ae94ccaae54809232dcf50fc70c11ecafa1
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-22 16:38:11 +0900

    feat: 1라운드에서 끝낸 경우 로직 구현(#194)

--- src/main/java/com/skyhorsemanpower/auction/quartz/AuctionClose.java
+++ src/main/java/com/skyhorsemanpower/auction/quartz/AuctionClose.java
@@ -8,6 +8,7 @@ import com.skyhorsemanpower.auction.domain.RoundInfo;
 import com.skyhorsemanpower.auction.kafka.KafkaProducerCluster;
 import com.skyhorsemanpower.auction.kafka.Topics;
 import com.skyhorsemanpower.auction.kafka.dto.AuctionCloseDto;
+import com.skyhorsemanpower.auction.quartz.data.MemberUuidsAndPrice;
 import com.skyhorsemanpower.auction.repository.AuctionCloseStateRepository;
 import com.skyhorsemanpower.auction.repository.AuctionHistoryRepository;
 import com.skyhorsemanpower.auction.repository.RoundInfoRepository;
@@ -42,13 +43,13 @@ public class AuctionClose implements Job {
         String auctionUuid = jobDataMap.getString("auctionUuid");
 
         // auction_close_state 도큐먼트에 acutionUuid 데이터가 있으면(마감됐으면) 바로 return
-        if(auctionCloseStateRepository.findByAuctionUuid(auctionUuid).isPresent()) {
+        if (auctionCloseStateRepository.findByAuctionUuid(auctionUuid).isPresent()) {
             log.info("Auction already close");
             return;
         }
 
         // auction_history 도큐먼트를 조회하여 경매 상태를 변경
-        if(auctionHistoryRepository.findFirstByAuctionUuidOrderByBiddingTimeDesc(auctionUuid).isEmpty()) {
+        if (auctionHistoryRepository.findFirstByAuctionUuidOrderByBiddingTimeDesc(auctionUuid).isEmpty()) {
             log.info("auction_history is not exist! No one bid the auction!");
 
             // 아무도 참여하지 않은 경우에는 auctionUuid와 auctionState(AUCTION_NO_PARTICIPANTS) 전송
@@ -84,26 +85,11 @@ public class AuctionClose implements Job {
                 findByAuctionUuidAndRoundOrderByBiddingTime(auctionUuid, round - 1);
         log.info("Before Last Round Auction History >>> {}", lastMinusOneRoundAuctionHistory.toString());
 
-        // 마지막 라운드 입찰자를 낙찰자로 고정
-        Set<String> memberUuids = new HashSet<>();
-        for(AuctionHistory auctionHistory : lastRoundAuctionHistory) {
-            memberUuids.add(auctionHistory.getBiddingUuid());
-        }
-
-        // 마지막 직전 라운드 입찰자 중 낙찰자 추가
-        for(AuctionHistory auctionHistory : lastMinusOneRoundAuctionHistory) {
-            // 동일 입찰자 제외하고 추가
-            memberUuids.add(auctionHistory.getBiddingUuid());
-
-            // 낙찰 가능 인원 수 만큼 리스트 추가
-            if (memberUuids.size() == numberOfParticipants) break;
-        }
-
-        log.info("memberUuids >>> {}", memberUuids.toString());
-
-        // 낙찰가는 마지막 이전 라운드에서 biddingPrice로 결정
-        BigDecimal price = lastMinusOneRoundAuctionHistory.get(0).getBiddingPrice();
-        log.info("price >>> {}", price);
+        // 낙찰가와 낙찰자 획득
+        MemberUuidsAndPrice memberUuidsAndPrice = getMemberUuidsAndPrice(
+                lastRoundAuctionHistory, lastMinusOneRoundAuctionHistory, numberOfParticipants);
+        Set<String> memberUuids = memberUuidsAndPrice.getMemberUuids();
+        BigDecimal price = memberUuidsAndPrice.getPrice();
 
         // 카프카로 경매 서비스 메시지 전달
         AuctionCloseDto auctionCloseDto = AuctionCloseDto.builder()
@@ -122,5 +108,51 @@ public class AuctionClose implements Job {
                 .auctionUuid(auctionUuid)
                 .auctionCloseState(true)
                 .build());
+    }
+
+    private MemberUuidsAndPrice getMemberUuidsAndPrice(List<AuctionHistory> lastRoundAuctionHistory,
+                                            List<AuctionHistory> lastMinusOneRoundAuctionHistory,
+                                            long numberOfParticipants) {
+        Set<String> memberUuids = new HashSet<>();
+        BigDecimal price;
+
+        // 1라운드에서 경매가 마감된 경우
+        if (lastMinusOneRoundAuctionHistory.isEmpty()) {
+            // 마지막 라운드 입찰자를 낙찰자로 고정
+            for (AuctionHistory auctionHistory : lastRoundAuctionHistory) {
+                memberUuids.add(auctionHistory.getBiddingUuid());
+            }
+
+            log.info("memberUuids >>> {}", memberUuids.toString());
+
+            // 낙찰가는 마지막 이전 라운드에서 biddingPrice로 결정
+            price = lastRoundAuctionHistory.get(0).getBiddingPrice();
+            log.info("price >>> {}", price);
+        }
+
+        // 1라운드 제외한 라운드에서 경매가 마감된 경우
+        else {
+            // 마지막 라운드 입찰자를 낙찰자로 고정
+            for (AuctionHistory auctionHistory : lastRoundAuctionHistory) {
+                memberUuids.add(auctionHistory.getBiddingUuid());
+            }
+
+            // 마지막 직전 라운드 입찰자 중 낙찰자 추가
+            for (AuctionHistory auctionHistory : lastMinusOneRoundAuctionHistory) {
+                // 동일 입찰자 제외하고 추가
+                memberUuids.add(auctionHistory.getBiddingUuid());
+
+                // 낙찰 가능 인원 수 만큼 리스트 추가
+                if (memberUuids.size() == numberOfParticipants) break;
+            }
+
+            log.info("memberUuids >>> {}", memberUuids.toString());
+
+            // 낙찰가는 마지막 이전 라운드에서 biddingPrice로 결정
+            price = lastMinusOneRoundAuctionHistory.get(0).getBiddingPrice();
+            log.info("price >>> {}", price);
+        }
+
+        return MemberUuidsAndPrice.builder().memberUuids(memberUuids).price(price).build();
     }
 }
