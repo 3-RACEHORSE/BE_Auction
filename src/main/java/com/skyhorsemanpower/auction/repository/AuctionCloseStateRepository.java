ea2c3bf477184dae54251b7b5a7d54cdbc8cfc2f
Chanwoong Park <128444378+chanchanwoong@users.noreply.github.com>
2024-06-27 23:49:59 +0900

    refactor: 동시성 문제를 해결하기 위해 mongoTemplate 적용(#231)

--- src/main/java/com/skyhorsemanpower/auction/repository/AuctionCloseStateRepository.java
+++ src/main/java/com/skyhorsemanpower/auction/repository/AuctionCloseStateRepository.java
@@ -1,12 +1,13 @@
 package com.skyhorsemanpower.auction.repository;
 
 import com.skyhorsemanpower.auction.domain.AuctionCloseState;
+import com.skyhorsemanpower.auction.repository.mongotemplate.CustomAuctionCloseStateRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AuctionCloseStateRepository extends MongoRepository<AuctionCloseState, String> {
+public interface AuctionCloseStateRepository extends MongoRepository<AuctionCloseState, String>, CustomAuctionCloseStateRepository {
     Optional<AuctionCloseState> findByAuctionUuid(String auctionUuid);
 }
